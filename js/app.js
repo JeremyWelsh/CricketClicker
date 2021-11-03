affd9fe4de0dce5118867c8e3555d40ff84f8b60
JeremyWelsh <JeremyWelsh>
2021-11-03 14:34:48 +1300

    Added rotary event js and updated config to allow rotary events

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 (function () {
+	
+	// Event listener for back key
 	window.addEventListener("tizenhwkey", function (ev) {
 		var activePopup = null,
 			page = null,
 			pageId = "";
-
+		
 		if (ev.keyName === "back") {
 			activePopup = document.querySelector(".ui-popup-active");
 			page = document.getElementsByClassName("ui-page-active")[0];
@@ -19,4 +21,32 @@
 			}
 		}
 	});
+	
+	
+	// Bezel Code ---------------------
+	// Following tutorial from
+	// https://developer.samsung.com/sdp/blog/en-us/2019/10/08/use-tizen-to-enable-galaxy-watch-rotary-events
+	// Checking bezel capabilities
+	var bezel = tizen.systeminfo.getCapability('http://tizen.org/feature/input.rotating_bezel');
+    console.log(bezel);
+    
+    // Event listener for Bezel
+    window.addEventListener('rotarydetent', function(ev) {
+        /* Get the direction value from the event */
+        var direction = ev.detail.direction;
+        var textbox = document.querySelector('.contents');
+        textbox = document.querySelector('#textbox');
+        
+        if (direction === 'CW') {
+            /* Add behavior for clockwise rotation */
+            console.log('clockwise');
+            textbox.innerHTML ="clockwise";
+        } else if (direction === 'CCW') {
+            //do nothing in case it was by accident
+        }
+    });
+	
+	
+	
+	
 }());
