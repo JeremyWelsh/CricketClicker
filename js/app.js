5025cdba78930fa2f10f0aff70053551ece0d604
JeremyWelsh <welshjm15@gmail.com>
2021-11-04 12:42:35 +1300

    Loads being counted by rotation, bug when reopening page, count not showing until rotation

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
+var loadCount = 0;
+
 (function () {
-	
 	// Event listener for back key
 	window.addEventListener("tizenhwkey", function (ev) {
 		var activePopup = null,
@@ -21,15 +22,12 @@
 			}
 		}
 	});
-	
-	
 	// Bezel Code ---------------------
 	// Following tutorial from
 	// https://developer.samsung.com/sdp/blog/en-us/2019/10/08/use-tizen-to-enable-galaxy-watch-rotary-events
 	// Checking bezel capabilities
 	var bezel = tizen.systeminfo.getCapability('http://tizen.org/feature/input.rotating_bezel');
     console.log(bezel);
-    
     // Event listener for Bezel
     window.addEventListener('rotarydetent', function(ev) {
         /* Get the direction value from the event */
@@ -39,14 +37,19 @@
         
         if (direction === 'CW') {
             /* Add behavior for clockwise rotation */
+        	loadCount++;
             console.log('clockwise');
-            textbox.innerHTML ="Started";
+            console.log('loadCount'+loadCount);
+            textbox.innerHTML = loadCount;
         } else if (direction === 'CCW') {
             //do nothing in case it was by accident
         }
     });
-	
-	
-	
-	
 }());
+
+window.addEventListener("load", function(){
+	console.log('loaded');
+	var textbox = document.querySelector('.contents');
+    textbox = document.querySelector('#textbox');
+    textbox.innerHTML = loadCount;
+});
