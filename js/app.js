696a2a81cdc066039d4fbfa6b4bd1123b8a84f1e
JeremyWelsh <welshjm15@gmail.com>
2021-11-15 14:42:04 +1300

    added reset button that works, now i need to design the ui

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
+///*global tau */
 var BallCount = 0,
-	OverCount = 0,
-	WicketCount = 0,
-	WideCount = 0;
- console.log("app started");
- /*
+    OverCount = 0,
+    WicketCount = 0,
+    ExtraCount = 0;
+console.log("app started");
+/*
  window.onload = function() {
 	 console.log("onload function running");
      var page = null,
@@ -17,68 +18,76 @@ var BallCount = 0,
      }
  };
 */
- // Event listener for back key
- window.addEventListener("tizenhwkey", function(ev) {
-     var activePopup = null,
-         page = null,
-         pageId = "";
-     if (ev.keyName === "back") {
-         activePopup = document.querySelector(".ui-popup-active");
-         page = document.getElementsByClassName("ui-page-active")[0];
-         pageId = page ? page.id : "";
-         var textbox = document.querySelector('.contents');
-         textbox = document.querySelector('#ballover');
-         if (pageId === "main" && !activePopup) {
-        	 if (BallCount >= 5) {
-                 OverCount++;
-                 BallCount = 0;
-             } else {
-                 BallCount++;
-             }
-             console.log('OverCount . BallCount ' + OverCount + "." + BallCount);
-             textbox.innerHTML = OverCount + "." + BallCount;
-         }
-     }
- });
+// Event listener for back key
+window.addEventListener("tizenhwkey", function(ev) {
+    var activePopup = null,
+        page = null,
+        pageId = "";
+    if (ev.keyName === "back") {
+        activePopup = document.querySelector(".ui-popup-active");
+        page = document.getElementsByClassName("ui-page-active")[0];
+        pageId = page ? page.id : "";
+        var textbox = document.querySelector('.contents');
+        textbox = document.querySelector('#ballover');
+        if (pageId === "main" && !activePopup) {
+            if (BallCount >= 5) {
+                OverCount++;
+                BallCount = 0;
+            } else {
+                BallCount++;
+            }
+            console.log('OverCount . BallCount ' + OverCount + "." + BallCount);
+            textbox.innerHTML = OverCount + "." + BallCount;
+        }
+    }
+});
 
- var bezel = tizen.systeminfo.getCapability('http://tizen.org/feature/input.rotating_bezel');
- console.log("has bezel: " + bezel); // checks if it has a bezel
+var bezel = tizen.systeminfo.getCapability('http://tizen.org/feature/input.rotating_bezel');
+console.log("has bezel: " + bezel); // checks if it has a bezel
 
- var cooldown = false;
+var cooldown = false;
 
- //Event listener for Bezel
- window.addEventListener('rotarydetent', function(ev) {
-     // Get the direction value from the event
-     var page = null,
-         pageId = "";
-     page = document.getElementsByClassName("ui-page-active")[0];
-     pageId = page ? page.id : "";
-     if (pageId === "main") {
-         var direction = ev.detail.direction;
-         var textboxw = document.querySelector('.contents');
-         textboxw = document.querySelector('#wicket');
-         var textboxe = document.querySelector('.contents');
-         textboxe = document.querySelector('#extra');
-         if (cooldown) {
-             return;
-         }
-         if (direction === 'CW') {
-             WicketCount++;
-             console.log('clockwise');
-             console.log('WicketCount'+ WicketCount);
-             //needs to be changed to a new textbox thing
-             textboxw.innerHTML = 'W: '+WicketCount;
-         } else if (direction === 'CCW') {
-        	 WideCount++;
-             console.log('anti-clockwise');
-             console.log('WideCount'+ WideCount);
-             //needs to be changed to a new textbox thing
-             textboxe.innerHTML = 'E: '+ WideCount;
-         }
-         cooldown = true;
-         setTimeout(function() {
-             cooldown = false;
-         }, 750);
+//Event listener for Bezel
+window.addEventListener('rotarydetent', function(ev) {
+    // Get the direction value from the event
+    var page = null,
+        pageId = "";
+    page = document.getElementsByClassName("ui-page-active")[0];
+    pageId = page ? page.id : "";
+    if (pageId === "main") {
+        var direction = ev.detail.direction;
+        var textboxw = document.querySelector('.contents');
+        textboxw = document.querySelector('#wicket');
+        var textboxe = document.querySelector('.contents');
+        textboxe = document.querySelector('#extra');
+        if (cooldown) {
+            return;
+        }
+        if (direction === 'CW') {
+            WicketCount++;
+            console.log('clockwise');
+            console.log('WicketCount' + WicketCount);
+            //needs to be changed to a new textbox thing
+            textboxw.innerHTML = 'W: ' + WicketCount;
+        } else if (direction === 'CCW') {
+            ExtraCount++;
+            console.log('anti-clockwise');
+            console.log('ExtraCount' + ExtraCount);
+            //needs to be changed to a new textbox thing
+            textboxe.innerHTML = 'E: ' + ExtraCount;
+        }
+        cooldown = true;
+        setTimeout(function() {
+            cooldown = false;
+        }, 750);
 
-     }
- });
+    }
+});
+
+function resetStats() {
+	console.log("reset stats");
+    BallCount = 0;
+    OverCount = 0;
+    WicketCount = 0;
+    ExtraCount = 0;
+}
