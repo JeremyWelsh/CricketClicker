13463bbd89629e638d26e03dabd5477719fafdc4
JeremyWelsh <welshjm15@gmail.com>
2021-11-16 14:24:02 +1300

    added timer cooldown for balls button

--- js/app.js
+++ js/app.js
@@ -4,32 +4,28 @@ var BallCount = 0,
     WicketCount = 0,
     ExtraCount = 0;
 console.log("app started");
-/*
- window.onload = function() {
-	 console.log("onload function running");
-     var page = null,
-         pageId = "";
-     page = document.getElementsByClassName("ui-page-active")[0];
-     pageId = page ? page.id : "";
-     if (pageId === "main") {
-         var textbox = document.querySelector('.contents');
-         textbox = document.querySelector('#textbox');
-         textbox.innerHTML = OverCount + "." + BallCount;
-     }
- };
-*/
+
+var bezel = tizen.systeminfo.getCapability('http://tizen.org/feature/input.rotating_bezel');
+console.log("has bezel: " + bezel); // checks if it has a bezel
+
+var cooldown = false;
+
 var textboxw = document.querySelector('.contents');
 textboxw = document.querySelector('#wicket');
 var textboxe = document.querySelector('.contents');
 textboxe = document.querySelector('#extra');
 var textbox = document.querySelector('.contents');
 textbox = document.querySelector('#ballover');
+
 // Event listener for back key
 window.addEventListener("tizenhwkey", function(ev) {
     var activePopup = null,
         page = null,
         pageId = "";
     if (ev.keyName === "back") {
+    	if (cooldown) {
+            return;
+        }
         activePopup = document.querySelector(".ui-popup-active");
         page = document.getElementsByClassName("ui-page-active")[0];
         pageId = page ? page.id : "";
@@ -43,13 +39,12 @@ window.addEventListener("tizenhwkey", function(ev) {
             console.log('OverCount . BallCount ' + OverCount + "." + BallCount);
             textbox.innerHTML = OverCount + "." + BallCount;
         }
+        cooldown = true;
+        setTimeout(function() {
+            cooldown = false;
+        }, 750);
     }
 });
-
-var bezel = tizen.systeminfo.getCapability('http://tizen.org/feature/input.rotating_bezel');
-console.log("has bezel: " + bezel); // checks if it has a bezel
-
-var cooldown = false;
 
 //Event listener for Bezel
 window.addEventListener('rotarydetent', function(ev) {
